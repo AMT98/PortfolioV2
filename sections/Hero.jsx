85b8f10cf0d635793efe096760519b326b666209
AMT98 <aswinmalla12@gmail.com>
2023-06-14 20:55:30 -0600

    Starting tomorrow

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -16,15 +16,15 @@ const Hero = () => (
     >
       <div className="flex justify-center items-center flex-col relative z-10">
         <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
-          Metaverse
+          Hi, my name is Aswin Malla
         </motion.h1>
         <motion.div
           variants={textVariant(1.2)}
           className="flex flex-row justify-center items-center"
         >
-          <h1 className={styles.heroHeading}>Ma</h1>
-          <div className={styles.heroDText} />
-          <h1 className={styles.heroHeading}>Ness</h1>
+          {/* <h1 className={styles.heroHeading}>Aswin Malla</h1> */}
+          {/* <div className={styles.heroDText} /> */}
+          <h1 className={styles.heroHeading}>Software Developer</h1>
         </motion.div>
       </div>
 
