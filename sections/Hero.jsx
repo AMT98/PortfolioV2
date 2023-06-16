d3412a2027f60572eee680fe0db158fd08f9be69
AMT98 <aswinmalla12@gmail.com>
2023-06-15 18:23:44 -0600

    Hero page semi-complete

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -71,9 +71,9 @@ const Hero = () => (
         <a href="#explore">
           <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
             <img
-              src="/stamp.png"
+              src="/resumeStamp.png"
               alt="stamp"
-              className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+              className="sm:w-[205px] w-[150px] sm:h-[155px] h-[150px] object-contain"
             />
           </div>
         </a>
