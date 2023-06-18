8cf2f6f2a331bed5cddacb9da4fa6e940d9120a8
AMT98 <aswinmalla12@gmail.com>
2023-06-17 22:14:35 -0600

    test

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -2,6 +2,8 @@
 
 import { motion } from 'framer-motion';
 
+import { FaGithub } from 'react-icons/fa';
+import { MdPreview } from 'react-icons/md';
 import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
@@ -24,18 +26,31 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       </h3>
     ) : (
       <div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
-        <div
-          className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
-        >
-          <img
-            src="/headset.svg"
-            alt="headset"
-            className="w-1/2 h-1/2 object-contain"
-          />
+        <div className="flex flex-row gap-6 justify-start items-center">
+
+          <div>
+
+            <div
+              className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
+            >
+              <FaGithub />
+            </div>
+            <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
+              Github
+            </p>
+          </div>
+          <div>
+
+            <div
+              className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
+            >
+              <MdPreview />
+            </div>
+            <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase ml-1">
+              Demo
+            </p>
+          </div>
         </div>
-        <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
-          Enter Metaverse
-        </p>
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
           {title}
         </h2>
