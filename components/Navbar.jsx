05287e1a840ba4c51058865e76f79b8ab7a478e2
AMT98 <aswinmalla12@gmail.com>
2023-06-19 13:41:00 -0600

    Only hamburger menu for nav items

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,6 @@ import { motion } from 'framer-motion';
 
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Link from 'next/link';
-import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
 const Navbar = () => {
@@ -39,41 +38,28 @@ const Navbar = () => {
       variants={navVariants}
       initial="hidden"
       whileInView="show"
-      className={`${styles.xPaddings} py-8 relative`}
+      className="py-8 relative pl-4 z-10"
     >
-      <div className="flex justify-between items-center w-full h-20 px-4 text-[#C2CCEB] bg-[#0A192F] fixed cursor-pointer z-10">
+      <div className="flex justify-between items-center w-full h-20 text-[#C2CCEB] bg-transparent fixed cursor-pointer z-10">
         <div>
           {/* <h1 className="text-5xl font-signature ml-2 text-[#0EC8AD]">AMT</h1> */}
           <img
             onClick={handleLogo}
-            className="w-12 h-auto ml-6 mt-3"
+            className="w-12 h-auto mt-3"
             src="/websiteLogo.png"
             alt="logo"
           />
         </div>
-        <ul className="hidden md:flex navItems">
-          {links.map(({ id, link }) => (
-            <li
-              key={id}
-              className="px-2 cursor-pointer capitalize text-sm hover:text-[#5CEACA] hover:scale-95 duration-200 text-[#C2CCEB]"
-            >
-              <Link href={link} smooth duration={500}>
-                <span className="px-1 text-[#5CEACA]">{id}.</span>
-                {link}
-              </Link>
-            </li>
-          ))}
-        </ul>
         <div
           onClick={() => setNav(!nav)}
-          className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+          className="cursor-pointer pr-4 z-10 text-gray-500"
         >
           {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
         </div>
         {nav && (
-        <ul className="ml-[23%] flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-[#0A192F] text-[#5CEACA]">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-white z-10 text-indigo-500">
           {links.map(({ id, link }) => (
-            <li key={id} className="cursor-pointer capitalize py-4 text-xl ml-[-23%]">
+            <li key={id} className="cursor-pointer capitalize py-4 text-xl">
               <Link
                 onClick={() => setNav(!nav)}
                 href={link}
