68a5e4f8d2f67edd0bfa082da33b60d6728835eb
AMT98 <aswinmalla12@gmail.com>
2023-06-20 15:01:27 -0600

    Added a favicon

--- app/head.js
+++ app/head.js
@@ -2,7 +2,7 @@ const Head = () => (
   <>
     <title>Aswin Malla</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/logo.svg" />
+    <link rel="icon" href="/favicon.ico" />
   </>
 );
 
