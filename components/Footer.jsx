6c633a6eb09b362fa5a9a3b4c3acd99721aba6d8
AMT98 <aswinmalla12@gmail.com>
2024-11-24 20:57:20 -0700

    Updated footer year

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,7 +17,6 @@ const Footer = () => (
   >
     <div className="footer-gradient" />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-
       <div className="flex flex-col">
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
@@ -64,20 +63,43 @@ const Footer = () => (
             </svg>
           </h4>
           <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2023 Aswin Malla. All rights reserved.
+            Copyright © 2024 Aswin Malla. All rights reserved.
           </p>
 
           <div className="flex gap-4">
-            <a href="https://www.github.com/AMT98" title="Github" target="_blank" className="border hover:animate-pulse rounded-full border-transparent flex bg-indigo-500 p-2 m-1" rel="noreferrer">
+            <a
+              href="https://www.github.com/AMT98"
+              title="Github"
+              target="_blank"
+              className="border hover:animate-pulse rounded-full border-transparent flex bg-indigo-500 p-2 m-1"
+              rel="noreferrer"
+            >
               <FaGithub size={25} color="white" />
             </a>
-            <a href="https://www.linkedin.com/in/aswinmalla/" target="_blank" title="Linkedin" className="border hover:animate-pulse rounded-full border-transparent flex bg-indigo-500 p-2 m-1" rel="noreferrer">
+            <a
+              href="https://www.linkedin.com/in/aswinmalla/"
+              target="_blank"
+              title="Linkedin"
+              className="border hover:animate-pulse rounded-full border-transparent flex bg-indigo-500 p-2 m-1"
+              rel="noreferrer"
+            >
               <FaLinkedinIn size={25} color="blue" />
             </a>
-            <a href="mailto: aswinmalla12@gmail.com" type="email" title="Email" className="border hover:animate-pulse rounded-full border-transparent flex bg-indigo-500 p-2 m-1">
+            <a
+              href="mailto: aswinmalla12@gmail.com"
+              type="email"
+              title="Email"
+              className="border hover:animate-pulse rounded-full border-transparent flex bg-indigo-500 p-2 m-1"
+            >
               <HiOutlineMail size={25} color="red" />
             </a>
-            <a href="/Resume_AswinMalla.pdf" download target="_blank" title="Download resume" className="border hover:animate-pulse rounded-full border-transparent flex bg-indigo-500 p-2 m-1">
+            <a
+              href="/Resume_AswinMalla.pdf"
+              download
+              target="_blank"
+              title="Download resume"
+              className="border hover:animate-pulse rounded-full border-transparent flex bg-indigo-500 p-2 m-1"
+            >
               <FcDocument size={25} />
             </a>
           </div>
